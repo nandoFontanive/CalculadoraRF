eaa93fd267a449d3e222eaf812c12b382c9ab667
nando.fontanive <nando.fontanive@gmail.com>
2022-09-28 18:45:02 -0300

    envio teste

--- js/calculadora.js
+++ js/calculadora.js
@@ -3,12 +3,6 @@ document.getElementById(capitalInvestido);
 document.getElementById(taxaJurosPoupanca);
 document.getElementById(quantidadeMeses);
 
-//montante = capital * (1 + taxa)^tempo
-//  var montante = capital * ;
-//https://mundoeducacao.uol.com.br/matematica/juros-compostos.htm
-
-
-
 function calculoJurosPoupanca(capitalInvestido, taxaJurosPoupanca, quantidadeMeses){
   capitalInvestido = document.getElementById('capitalInvestido').value;
   taxaJurosPoupanca = document.getElementById('taxaJurosPoupanca').value;
@@ -31,28 +25,50 @@ function calculoJurosPoupanca(capitalInvestido, taxaJurosPoupanca, quantidadeMes
      elemResult.innerNumber = "O resultado é " + Number(calculoJurosPoupanca);
   }
 */
-function calcular(capitalInvestido, taxaJurosPoupanca, quantidadeMeses) {
-  capitalInvestido = Number(document.getElementById('capitalInvestido').value);
-  taxaJurosPoupanca = Number(document.getElementById('taxaJurosPoupanca').value);
-  quantidadeMeses = Number(document.getElementById('quantidadeMeses').value);
-  
-  elemResult = document.getElementById('montantePoupanca');
+function calcularPoupanca() {
+  const capitalInvestido = Number(document.getElementById('capitalInvestido').value);
+  const taxaJurosPoupanca = Number(document.getElementById('taxaJurosPoupanca').value);
+  const quantidadeMeses = Number(document.getElementById('quantidadeMeses').value);
+  console.log({capitalInvestido, taxaJurosPoupanca, quantidadeMeses})
+  const elemResult = document.getElementById('montantePoupanca');
 
-  montantePoupanca = capitalInvestido * (Math.pow((1 + (taxaJurosPoupanca * 12)/100), quantidadeMeses/12));
-  
+  const montantePoupanca = capitalInvestido * (Math.pow((1 + (taxaJurosPoupanca * 12)/100), quantidadeMeses/12));
   elemResult.innerText = (montantePoupanca.toFixed(2));
   }
 
-function calcularLCI(capitalInvestido, taxaJurosPoupanca, quantidadeMeses) {
+function calcularLCI() {
   capitalInvestido = Number(document.getElementById('capitalInvestido').value);
+  taxaDI = Number(document.getElementById('taxaDI').value);
   taxaLCI = Number(document.getElementById('taxaLCI').value);
   quantidadeMeses = Number(document.getElementById('quantidadeMeses').value);
   
-  taxaLCI = taxaLCI * 
-
   elemResult = document.getElementById('montanteLCI');
 
-  montantePoupanca = capitalInvestido * (Math.pow((1 + (taxaLCI * 12)/100), quantidadeMeses/12));
-  
+  montanteLCI = capitalInvestido * (Math.pow((1 + ((taxaLCI/100) * taxaDI)), quantidadeMeses/12));
   elemResult.innerText = (montanteLCI.toFixed(2));
+}
+
+function calcularCDB(){
+  capitalInvestido = Number(document.getElementById('capitalInvestido').value);
+  taxaDI = Number(document.getElementById('taxaDI').value);
+  taxaCDB = Number(document.getElementById('taxaCDB').value);
+  quantidadeMeses = Number(document.getElementById('quantidadeMeses').value);
+  
+  elemResult = document.getElementById('montanteCDB');
+  
+  montanteCDB = capitalInvestido * (Math.pow((1 + ((taxaCDB/100) * taxaDI)), quantidadeMeses/12));
+  
+  if (quantidadeMeses < 6){
+    montanteCDB = montanteCDB * 0.775;
   }
+    else if (quantidadeMeses >= 6 && quantidadeMeses < 12){
+      montanteCDB = montanteCDB * 0.8;
+  }
+    else if (quantidadeMeses >=12 && quantidadeMeses < 24){
+      montanteCDB = montanteCDB * 0.825;
+  }
+    else {
+      montanteCDB = montanteCDB * 0.85;
+  }
+  elemResult.innerText = (montanteCDB.toFixed(2));
+}
