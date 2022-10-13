833a8740afb6ab0c5695bb81b5a2784379355ef8
unknown <you@example.com>
2022-10-13 15:19:24 -0300

    correção funções

--- js/calculadora.js
+++ js/calculadora.js
@@ -26,19 +26,21 @@ function calcular(tipo) {
   calcularCDB(capitalInvestido, taxaDI, taxaCDB, quantidadeMeses);
 }
 
-//precisa chamar parâmetro quantidadeMeses aqui e na linha 37?
-function calcularFoo2(quantidadeMeses) {
+//pq precisa chamar parâmetro quantidadeMeses aqui e nas linhas 37/68/80 tb?
+function calcularMeses(quantidadeMeses) {
   return quantidadeMeses / 12;
 }
 
 function calcularCDB(capitalInvestido, taxaDI, taxaCDB, quantidadeMeses) {
   
-  const foo1 = (1 + ((taxaCDB / 100) * taxaDI));
-  const foo2 = calcularFoo2(quantidadeMeses);
-  const foo3 = (Math.pow(foo1), foo2) - 1;
+  const cdb1 = (1 + ((taxaCDB / 100) * taxaDI));
+  const cdb2 = calcularMeses(quantidadeMeses);
+  const cdb3 = Math.pow(cdb1, cdb2) - 1;
 
-  const rendimentoCDBBruto = capitalInvestido * foo3;
+  const rendimentoCDBBruto = capitalInvestido * cdb3;
   const rendimentoCDBLiquido = calcularCDBLiquido(rendimentoCDBBruto, quantidadeMeses)
+  
+  //rendimentoCDB = capitalInvestido * (Math.pow((1 + ((taxaCDB/100) * taxaDI)), quantidadeMeses/12)-1)
 
   const finalCDB = capitalInvestido + rendimentoCDBLiquido;
   resultadoCDB = document.getElementById('montanteCDB');
@@ -62,58 +64,24 @@ function calcularCDBLiquido(rendimentoCDBBruto, quantidadeMeses) {
 }
 
 function calcularPoupanca(capitalInvestido, taxaJurosPoupanca, quantidadeMeses) {
-  /*
-  const fooP1 = (1 + taxaJurosPoupanca / 100)
-  const fooP2 = calcularFoo2(quantidadeMeses);
-  const fooP3 = (Math.pow(fooP1), fooP2);
+  const poupanca1 = (1 + taxaJurosPoupanca / 100);
+  const poupanca2 = calcularMeses(quantidadeMeses);
+  const poupanca3 = Math.pow(poupanca1, quantidadeMeses);
 
-  const montantePoupanca = capitalInvestido * fooP3;*/
-  const montantePoupanca = capitalInvestido * (Math.pow(1 + taxaJurosPoupanca / 100, quantidadeMeses));
+  const montantePoupanca = capitalInvestido * poupanca3;
+  //const montantePoupanca = capitalInvestido * (Math.pow(1 + taxaJurosPoupanca / 100), quantidadeMeses);
+  
   resultadoPoupanca = document.getElementById('montantePoupanca');
   resultadoPoupanca.innerText = (new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(montantePoupanca));
 }
 
 function calcularLCI(capitalInvestido, taxaDI, taxaLCI, quantidadeMeses) {
-  const fooL1 = (1 + (taxaLCI / 100) * taxaDI);
-  const fooL2 = calcularFoo2(quantidadeMeses);
-  const fooL3 = (Math.pow(fooL1), fooL2);
+  const lci1 = (1 + (taxaLCI / 100) * taxaDI);
+  const lci2 = calcularMeses(quantidadeMeses);
+  const lci3 = Math.pow(lci1, lci2);
 
-  const montanteLCI = capitalInvestido * fooL3;
+  const montanteLCI = capitalInvestido * lci3;
+  //const montanteLCI = capitalInvestido * (Math.pow((1 + ((taxaLCI/100) * taxaDI)), quantidadeMeses/12));
   resultadoLCI = document.getElementById('montanteLCI');
   resultadoLCI.innerText = (new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(montanteLCI));
 }
-
-
-
-
-/*
-slider
-const rangeInputs = document.querySelectorAll('input[type="range"]')
-const numberInput = document.getElementById('capitalInvestido')
-
-function handleInputChange(e) {
-  let target = e.target
-  if (e.target.type !== 'range') {
-    target = document.getElementById('range')
-  }
-  const min = target.min
-  const max = target.max
-  const val = target.value
-
-  target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%'
-}
-
-rangeInputs.forEach(input => {
-  input.addEventListener('input', handleInputChange)
-})
-
-numberInput.addEventListener('input', handleInputChange)
-*/
-
-
-
-/*
-  const montantePoupanca = capitalInvestido * (Math.pow(1 + taxaJurosPoupanca / 100, quantidadeMeses));
-  montanteLCI = capitalInvestido * (Math.pow((1 + ((taxaLCI/100) * taxaDI)), quantidadeMeses/12));
-  rendimentoCDB = capitalInvestido * (Math.pow((1 + ((taxaCDB/100) * taxaDI)), quantidadeMeses/12)-1)
-*/
